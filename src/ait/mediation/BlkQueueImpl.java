42beb92a37af0b2629dba30f25860de30d8f2c2d
Viktoriya-Frank <playviktory@gmail.com>
2025-04-22 22:31:02 +0200

    homework: push and pop methods

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,47 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+    private final Queue<T> queue;
+    private final int maxSize;
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.maxSize = maxSize;
+        this.queue = new LinkedList<>();
     }
 
     @Override
     public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        synchronized (this) {
+            while (queue.size() >= maxSize) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+            queue.add(message);
+            notifyAll();
+        }
     }
 
     @Override
     public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return null;
+                }
+            }
+            T result = queue.poll();
+            notifyAll();
+            return result;
+        }
     }
 }
